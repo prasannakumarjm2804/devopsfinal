4fffdd8aab7ca1f1ffe86f534e20256113d8d128
PRASANNAKUMAR J M <112399618+prasannakumarjm2804@users.noreply.github.com>
2025-04-03 14:12:21 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 echo hi123
 chmod +x build.sh
 ./build.sh
-docker login -u deepan47 -p Deepan11032004
+docker login -u prasannakumarjm -p sh0f0oj23j
 # Use non-interactive login
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 # Tag and push the image
-docker tag car-app deepan47/car-app
-docker push deepan47/car-app
-docker pull deepan47/car-app
-docker run -d -p 8000:80 --name=my-service-repo deepan47/car-app
+docker tag shop prasannakumarjm/shop
+docker push prasannakumarjm/shop
+docker pull prasannakumarjm/shop
+docker run -d -p 8000:80 --name=my-service-repo prasannakumarjm/shop
 
